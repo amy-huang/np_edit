0e409bd1c7b569dd5652b9f878f3ef199b406145
Amy <amy.jiamei.huang@gmail.com>
2017-07-17 11:27:13 -0700

    changed inflow frequency in randgen to match parallel version. put in cell picking and random cell introduction.

--- randgen2.c
+++ randgen2.c
@@ -269,7 +269,7 @@
 /* How frequently should random cells / energy be introduced?
  * Making this too high makes things very chaotic. Making it too low
  * might not introduce enough energy. */
-#define INFLOW_FREQUENCY 100
+#define INFLOW_FREQUENCY 160
 
 /* Base amount of energy to introduce per INFLOW_FREQUENCY ticks */
 #define INFLOW_RATE_BASE 4000
